6314fa4c9c09a7dca9d0a1149917c64ae2d4d64a
Alex Twin <alex.twintheone@gmail.com>
2026-04-24 14:51:29 -0600

    qol

--- TEMPESTCore/Scriptable Objects/StyleActionSO.cs
+++ TEMPESTCore/Scriptable Objects/StyleActionSO.cs
@@ -10,6 +10,7 @@ namespace TEMPESTCore
     {
         [Tooltip("What the Style Action is being registered as i.e wideWade.Discombobulated")]
         public string styleID = "wideWade.StyleActionExample";
+        [TextArea(2,4)]
         [Tooltip("The Rich text of the Style Action (What it'll look like on the UI): '<color=red>DISCOMBOBULATED</color>'")]
         public string richStyleText = "<color=red>WIDESTYLEACTIONEXAMPLE</color>";
         [Tooltip("How much points thsi style action gives")]
